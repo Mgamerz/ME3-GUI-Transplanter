28fba24e2ac16d05332fb9035d9b0dc0324cfdd5
Mgamerz <mgamerz2@gmail.com>
2016-01-21 10:30:53 -0700

    Added mock up GUI

--- ME3 GUI Transplanter/Transplanter-GUI/Form1.Designer.cs
+++ ME3 GUI Transplanter/Transplanter-GUI/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.srcTextField = new System.Windows.Forms.TextBox();
+            this.srcfileBrowseButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.destTextField = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Transplant";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.srcfileBrowseButton);
+            this.groupBox1.Controls.Add(this.srcTextField);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 47);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source File";
+            // 
+            // srcTextField
+            // 
+            this.srcTextField.Location = new System.Drawing.Point(6, 19);
+            this.srcTextField.Name = "srcTextField";
+            this.srcTextField.Size = new System.Drawing.Size(372, 20);
+            this.srcTextField.TabIndex = 3;
+            // 
+            // srcfileBrowseButton
+            // 
+            this.srcfileBrowseButton.Location = new System.Drawing.Point(384, 17);
+            this.srcfileBrowseButton.Name = "srcfileBrowseButton";
+            this.srcfileBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.srcfileBrowseButton.TabIndex = 1;
+            this.srcfileBrowseButton.Text = "Browse...";
+            this.srcfileBrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.destTextField);
+            this.groupBox2.Location = new System.Drawing.Point(12, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(469, 47);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Destination File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // destTextField
+            // 
+            this.destTextField.Location = new System.Drawing.Point(6, 19);
+            this.destTextField.Name = "destTextField";
+            this.destTextField.Size = new System.Drawing.Size(372, 20);
+            this.destTextField.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(171, 118);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(219, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 123);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(117, 13);
+            this.statusLabel.TabIndex = 10;
+            this.statusLabel.Text = "10/50 exports scanned";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 155);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button srcfileBrowseButton;
+        private System.Windows.Forms.TextBox srcTextField;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox destTextField;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
